98bd0bd480d48b3f5f92e12c186aa8f5a41cf143
MinyoungJun <minyoung9353@gmail.com>
2014-05-11 23:31:40 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Giverunner::Application.config.secret_key_base = 'afd64e7ec568ec491d42af22d757cca9bf239f039f06f1dc2991992407de99935237100b9d01be2b3667e843465372d604b32e858663656d451949e0752b76e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Giverunner::Application.config.session_store :cookie_store, key: '_giverunner_session'
